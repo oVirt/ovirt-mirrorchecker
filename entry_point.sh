2d071a2f9f0c8f4ba440b53ce8160d1e015970d5
ngoldin <ngoldin@redhat.com>
2016-09-03 00:26:38 +0000

    .

--- entry_point.sh
+++ entry_point.sh
@@ -5,7 +5,4 @@ envsubst < /mirrorchecker/passwd.template > /tmp/passwd
 export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
-cp /mirrorchecker/.ssh/ssh-private-key /mirrorchecker/.ssh/id_rsa
-chown mirrorchecker:mirrorchecker /mirrorchecker/.ssh/id_rsa
-chmod 600 /mirrorchecker/.ssh/id_rsa
 exec "$@"
